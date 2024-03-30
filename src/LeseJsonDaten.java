3f932ed0623daf9159f3a593cb2aa96642d393ad
Frank Hilgenberg <frank.hilgenberg@franks-divecenter.de>
2024-03-30 16:22:49 +0100

    Einige Änderungen

--- src/LeseJsonDaten.java
+++ src/LeseJsonDaten.java
@@ -16,84 +16,97 @@ import java.io.IOException;
 public class LeseJsonDaten {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		String formatierung = "%-35s %s %s";
-		String trennZeichen = ":";
-		
-		String dateiMitJsonDaten = "src/daten.json";	
-		
-		File datei = new File(dateiMitJsonDaten);
-		System.out.println(String.format(formatierung, "<1> Erzeugt mit File", trennZeichen, datei));
-		System.out.println(String.format(formatierung, "<2> Erzeugt mit .getName()", trennZeichen, datei.getName()));
-		System.out.println(String.format(formatierung, "<3> Erzeugt mit .getAbsolutePath()", trennZeichen, datei.getAbsolutePath()));
-		System.out.println(String.format(formatierung, "<4> Erzeugt mit .getcanRead()", trennZeichen, datei.canRead()));
-		System.out.println(String.format(formatierung, "<5> Erzeugt mit .length()", trennZeichen, datei.length()));
-		zeichneTrennlinie();
-		
-		FileInputStream fis = new FileInputStream(datei);		
-		System.out.println(String.format(formatierung, "<6> Erzeugt mit FileInputStream", trennZeichen, fis));
-		zeichneTrennlinie();
-
-		Scanner scan = new Scanner(fis);		
-		System.out.println(String.format(formatierung, "<7> Erzeugt mit Scanner", trennZeichen,  scan.toString()));
-		zeichneTrennlinie();
-		scan.close();
-		
-//		JSONObject jo = new JSONObject(datei);
-//		System.out.println(String.format(formatierung, "<8> Erzeugt mit JSONObject", trennZeichen, jo.getInt("id")));
-//		zeichneTrennlinie();
-//		
-		InputStream is = new FileInputStream(dateiMitJsonDaten);
-		JSONObject objekt = new JSONObject(is);
-		System.out.println(String.format(formatierung, "<8> Erzeugt mit JSONObject", trennZeichen, objekt));
-		Person person = new Person();
-				
-		//person.setVorname(objekt.getString("vorname"));
-		
-		zeichneTrennlinie();
-		
+			
+		zeichneTrennlinie(2);		
 		JSONObject json = new JSONObject()
 			    .put("nummer", 1)
 			    .put("vorname", "Klaus")
 			    .put("nachname", "Mustermann")
-				.put("alter", 54);
-		
-		System.out.println("<9> " + json);
-		zeichneTrennlinie();
-
-		
-		String wert = json.optString("nummer"); // -> "1"
-		System.out.println("Wert von nummer: " + wert);	
-		
+				.put("alter", 54)	
+				.put("sprachen", "Deutsch");		
+		System.out.println("Ausgabe eines JSON-Objekts:");
+		System.out.println(json);
+		zeichneTrennlinie(2);		
+			
 		try
 		{
 			// JSON-Datei einlesen
-			String jsonDaten = new String(Files.readAllBytes(Paths.get(dateiMitJsonDaten)));
+			String jsonDatei = "src/daten.json";
+			String jsonDaten = new String(Files.readAllBytes(Paths.get(jsonDatei)));
+			
+			// JSON-Objekt erstellen		
 			JSONObject jsonObj = new JSONObject(jsonDaten);
 			
 			// JSON-Inhalte verarbeiten
+			String id = jsonObj.optString("id");
             String vorname = jsonObj.getString("vorname");
             String nachname = jsonObj.getString("nachname");
-            int alter = jsonObj.getInt("alter");
-            System.out.println(vorname + " " + nachname + " ist morgen " + alter + " Jahr alt!");
+            String alter = jsonObj.optString("alter");
+            String sprachen = jsonObj.getString("sprachen");
+            
+            int zeichenID = id.length();
+            int zeichenVorname = vorname.length();
+            int zeichenNachname = nachname.length();
+            int zeichenAlter = 5;
+            int zeichenSprachen = sprachen.length();
+
+            
+            System.out.println("Anzahl der Zeichen von ID: " + zeichenID + "\n");
+            
+            String formatierung = "| %-" + zeichenID + "s | %-" + zeichenVorname + "s | %-" + zeichenNachname + "s | %-" + zeichenAlter + "s | %-" + zeichenSprachen + "s |";
+            
+            // Declaring ANSI_RESET so that we can reset the color 
+            final String ANSI_RESET = "\u001B[0m"; 
+          
+            // Declaring the color 
+            // Custom declaration 
+            final String ANSI_YELLOW = "\u001B[33m"; 
+            
+            System.out.println(ANSI_YELLOW + "Liste aller Personen" + ANSI_RESET);
+            
+            linieZeichnen(zeichenID, zeichenVorname, zeichenNachname, zeichenAlter, zeichenSprachen);    		
+    		System.out.println(String.format(formatierung, "ID", "Vorname", "Nachname", "Alter", "Sprachen"));
+    		linieZeichnen(zeichenID, zeichenVorname, zeichenNachname, zeichenAlter, zeichenSprachen);
+    		System.out.println(String.format(formatierung, id, vorname, nachname, alter, sprachen));
+    		linieZeichnen(zeichenID, zeichenVorname, zeichenNachname, zeichenAlter, zeichenSprachen);
+    		System.out.println(String.format(formatierung, 4711, "Susi", "Sonnenschein", 18, "Englisch"));
+    		linieZeichnen(zeichenID, zeichenVorname, zeichenNachname, zeichenAlter, zeichenSprachen);
+    		System.out.println(String.format(formatierung, 1234, "Klaus", "Meier", 33, "Deutsch"));
+    		linieZeichnen(zeichenID, zeichenVorname, zeichenNachname, zeichenAlter, zeichenSprachen);
+    		  		
+            
             
 		}
 		catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		// JSON-Objekt erstellen
-		
-		
-		
-		
 	}
 	
-	private static void zeichneTrennlinie()
+	private static void linieZeichnen(int zeichenID, int zeichenVorname, int zeichenNachme, int zeichenAlter, int zeichenSprachen)
+	{
+		String linie = "";
+		String kreuz = "+";
+		String strich = "-";
+		
+		linie  = kreuz + strich.repeat(zeichenID+2);
+		linie += kreuz + strich.repeat(zeichenVorname+2);
+		linie += kreuz + strich.repeat(zeichenNachme+2);
+		linie += kreuz + strich.repeat(zeichenAlter+2);
+		linie += kreuz + strich.repeat(zeichenSprachen+2);
+		linie += kreuz;
+		System.out.println(linie);
+	}
+	
+	private static void zeichneTrennlinie(int anzahlLinien)
 	{
 		String zeichen = "-";
-		int anzahl = 100;
-		System.out.println(zeichen.repeat(anzahl));
+		int anzahlZeichen = 90;
+		String linie = zeichen.repeat(anzahlZeichen);
+				
+		for (int i = 0; i < anzahlLinien; i++)
+		{
+			System.out.println(linie);
+		}
 	}	
-	
-	
 }
